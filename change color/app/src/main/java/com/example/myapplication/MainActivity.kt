ac0957b0318ffbd4034a3581df77c5b90c89cb5b
hoangcuteday <109096454+hoangcuteday@users.noreply.github.com>
2024-02-26 14:08:59 +0700

    add app change color

--- change color/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ change color/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.example.myapplication
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+
+    private val colors = listOf(Color.RED, Color.BLUE, Color.YELLOW, Color.GREEN, Color.MAGENTA, Color.DKGRAY)
+
+    private lateinit var myButton: FloatingActionButton
+    private lateinit var myView: View
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        myView = findViewById(R.id.view)
+        myButton = findViewById(R.id.myButton_change)
+
+        myButton.setOnClickListener {
+            myView.setBackgroundColor(colors[Random.nextInt(colors.size)])
+        }
     }
 }
