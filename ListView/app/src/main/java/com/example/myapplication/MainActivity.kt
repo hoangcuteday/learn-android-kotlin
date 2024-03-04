75311a9e8433785821ac2905e5a2e324b70546c1
hoangcuteday <109096454+hoangcuteday@users.noreply.github.com>
2024-03-05 02:42:29 +0700

    add learn list view basic

---
+++ ListView/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import androidx.appcompat.app.AppCompatActivity
+import com.example.myapplication.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        val mutableList: MutableList<String> = mutableListOf("hoang", "thien", "hihi", "hehe")
+
+        binding.listView.adapter = ArrayAdapter(
+            this,
+            android.R.layout.simple_list_item_1,
+            mutableList
+        )
+
+        binding.button.setOnClickListener {
+            mutableList.add(binding.edit.text.toString())
+            binding.listView.adapter = ArrayAdapter(
+                this,
+                android.R.layout.simple_list_item_1,
+                mutableList
+            )
+        }
+    }
+}
